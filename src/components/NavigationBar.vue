968cf26b3b1814e581c41d1f5ca97bf982ff5cc6
Luca Xue <lxue02@gmail.com>
2022-01-03 14:00:39 +0000

    Update navigation bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -6,32 +6,31 @@ const { t } = useI18n()
 <template>
   <nav class="mb-6 w-full flex justify-between">
     <router-link class="icon-btn mx-2" to="/" :title="t('button.home')">
-      <span class="font-semibold">Luca Xue</span>
+      <uil-home-alt class="mt-1" />
     </router-link>
-    <div class="flex">
-      <router-link class="icon-btn mx-2" to="/about" :title="t('button.home')">
-        About
-      </router-link>
 
-      <router-link class="icon-btn mx-2" to="/blogs" :title="t('button.home')">
-        Blogs
+    <div class="flex">
+      <router-link class="icon-btn mx-2" to="/posts" :title="t('button.home')">
+        <span class="sm:block hidden">Blog</span>
+        <uil-file-alt class="sm:hidden mt-1" />
       </router-link>
 
       <router-link class="icon-btn mx-2" to="/projects" :title="t('button.home')">
-        Projects
+        <span class="sm:block hidden">Projects</span>
+        <uil-lightbulb-alt class="sm:hidden mt-1" />
       </router-link>
 
       <a class="icon-btn mx-2" rel="noreferrer" href="https://twitter.com/luca_xue" target="_blank" title="Twitter">
-        <carbon-logo-twitter />
+        <uil-twitter-alt class="mt-1" />
       </a>
 
       <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/lucaxue" target="_blank" title="GitHub">
-        <carbon-logo-github />
+        <uil-github-alt class="mt-1" />
       </a>
 
       <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-        <carbon-moon v-if="isDark" />
-        <carbon-sun v-else />
+        <uil-moon v-if="isDark" class="mt-1" />
+        <uil-sun v-else class="mt-1" />
       </button>
     </div>
   </nav>
