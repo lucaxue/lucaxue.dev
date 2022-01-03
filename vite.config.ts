8f7a5006dcaf0a5e07fe68b684154967f1b0599c
Luca Xue <lxue02@gmail.com>
2022-01-03 13:11:46 +0000

    Change prose size to default

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ import Inspect from 'vite-plugin-inspect'
 import Prism from 'markdown-it-prism'
 import LinkAttributes from 'markdown-it-link-attributes'
 
-const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
+const markdownWrapperClasses = 'prose m-auto text-left'
 
 export default defineConfig({
   resolve: {
