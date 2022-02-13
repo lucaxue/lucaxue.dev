7c3462faa29be92d77f4441bd9b18a2953a6cad5
Luca Xue <lxue02@gmail.com>
2022-02-13 16:51:59 +0000

    feat: make footer year dynamic

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer class="px-7 mt-10 prose m-auto opacity-50 flex">
+  <footer class="flex m-auto mt-10 prose opacity-50 px-7">
     <span class="text-sm">
       <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
         CC BY-NC-SA 4.0
       </a>
-      2022 © Luca Xue
+      {{ new Date().getFullYear() }} © Luca Xue
     </span>
   </footer>
 </template>
